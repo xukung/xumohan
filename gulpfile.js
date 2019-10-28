3c15f04d52ad8a7ae6ef6dfeaa2a3748577c6dbb
xukun <xukun@huanqiu.com>
2019-10-28 16:25:35 +0800

    sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
@@ -8,16 +9,20 @@ var cssnano = require('cssnano');
 const { series, watch, src, dest } = require('gulp');
 
 
-watch(['./public/src/scss/*.scss'], { ignoreInitial: false }, function scssToCss (cb) {
+watch(['./public/src/scss/*.scss'], { ignoreInitial: false }, function scssToCss(cb) {
     var processors = [
         autoprefixer,
         cssnano,
     ];
     return src('./public/src/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(processors))
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compact',  //[compressed,compact,nested,expanded]
+        }).on('error', sass.logError))
+        // .pipe(postcss(processors))
+        .pipe(sourcemaps.write())
         .pipe(dest('./public/dist/css'));
-    
+
     cb();
 });
 
