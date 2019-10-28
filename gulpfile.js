572cd4bd56a8cef818cbd1e7f536cb6db84fc483
xukun <xukun@huanqiu.com>
2019-10-28 15:22:33 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,46 +5,24 @@ var sass = require('gulp-sass');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 
-const { series } = require('gulp');
-
-// gulp.task('watch', function () {
-//     gulp.watch('./public/src/scss/*.scss', ['sass']);
-// });
+const { series, watch, src, dest } = require('gulp');
 
 
-// gulp.task('sass', function () {
-//     var processors = [
-//         autoprefixer,
-//         cssnano,
-//     ];
-//     return gulp.src('./public/src/scss/*.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(postcss(processors))
-//         .pipe(gulp.dest('./public/dist/css'));
-// });
-
-gulp.task('watch', function () {
-    gulp.watch('./public/src/scss/*.scss', ['sass']);
-});
-
-gulp.task('sass', function () {
+watch(['./public/src/scss/*.scss'], { ignoreInitial: false }, function scssToCss (cb) {
     var processors = [
         autoprefixer,
         cssnano,
     ];
-    return gulp.src('./public/src/scss/*.scss')
+    return src('./public/src/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(processors))
-        .pipe(gulp.dest('./public/dist/css'));
+        .pipe(dest('./public/dist/css'));
+    
+    cb();
 });
 
 function defaultTask(cb) {
-    // place code for your default task here
-
-    console.log('abc');
-    
-
     cb();
-  }
+}
 
-exports.default = defaultTask;
+exports.default = series(defaultTask);
